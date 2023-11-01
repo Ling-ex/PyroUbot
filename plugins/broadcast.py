3010fa0f2e6ad1b82869d071f6c94e2f3f0878e0
Ling-ex <sahrulgunawangugun@gmail.com>
2023-11-01 15:46:12 +0700

    add fitur ucast and fwdcast

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -22,13 +22,12 @@ def get_msg(m):
 
 
 @Client.on_message(filters.command(["broadcast", "gcast"], config.prefix) & filters.me)
-async def broadcast(c: Client, m: types.Message):
+async def broadcast_group(c: Client, m: types.Message):
     msg = get_msg(m)
     if not msg:
         return await m.reply("Give me a message or reply to a message!")
-    load = await m.reply("Broadcast processing....")
-    done = 0
-    error = 0
+    load = await m.reply("Broadcast Group processing....")
+    done = error = 0
     async for dialog in c.get_dialogs():
         if dialog.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
             chat_id = dialog.chat.id
@@ -43,4 +42,45 @@ async def broadcast(c: Client, m: types.Message):
                 await asyncio.sleep(f.value)
             except Exception:
                 error += 1
-    await load.edit(f"<i>Broadcast was sent to {done} groups, failed to send to {error} groups</i>")
+    return await load.edit(f"<i>Broadcast was sent to {done} groups, failed to send to {error} groups</i>")
+
+@Client.on_message(filters.command("ucast", config.prefix) & filters.me)
+async def broadcast_users(c: Client, m: types.Message):
+    msg = get_msg(m)
+    if not msg:
+        return await m.reply("Give me a message or reply to a message!")
+    load = await m.reply("Broadcast users processing.....")
+    done = error = 0
+    async for dialog in c.get_dialogs():
+        if dialog.chat.type in enums.ChatType.PRIVATE:
+            chat_id = dialog.chat.id
+            try:
+                if m.reply_to_message:
+                    await msg.copy(chat_id)
+                else:
+                    await c.send_message(chat_id, msg)
+                done += 1
+                await asyncio.sleep(0.5)
+            except errors.FloodWait as f:
+                await asyncio.sleep(f.value)
+            except Exception:
+                error += 1
+    return await load.edit(f"<i>Broadcast was send to {done} users, failed to send to {error} users</i>")
+
+@Client.on_message(filters.command("fwdcast", config.prefix) & filters.me)
+async def broadcast_forward(c: Client, m: types.Message):
+    if not (reply := m.reply_to_message):
+        return await m.reply("Reply to messages you want to continue posting")
+    load = await m.reply("Broadcast forward processing....")
+    done = error = 0
+    async for dialog in c.get_dialogs():
+        if dialog.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
+            chat_id = dialog.chat.id
+            try:
+                await c.send_message(chat_id, reply)
+                done += 1
+            except errors.FloodWait as f:
+                await asyncio.sleep(f.value)
+            except Exception:
+                error += 1
+    return await load.edit(f"<i>Broadcast was send to {done} forward, failed to send to {error} forward")
