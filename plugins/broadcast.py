6acdfad6666c9281b174a9cc7cef220272f5f47b
KangerOnly <117170541+Ling-ex@users.noreply.github.com>
2024-01-23 12:42:42 +0700

    fixed broadcast forward and for send loops in all broadcast features  loops

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -28,21 +28,31 @@ async def broadcast_group(c: Client, m: types.Message):
         return await m.reply("Give me a message or reply to a message!")
     load = await m.reply("Broadcast Group processing....")
     done = error = 0
+    chat_ids = []
+
     async for dialog in c.get_dialogs():
         if dialog.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-            chat_id = dialog.chat.id
-            try:
-                if m.reply_to_message:
-                    await msg.copy(chat_id)
-                else:
-                    await c.send_message(chat_id, msg)
-                done += 1
-                await asyncio.sleep(0.5)
-            except errors.FloodWait as f:
-                await asyncio.sleep(f.value)
-            except Exception:
-                error += 1
-    return await load.edit(f"<i>Broadcast was sent to {done} groups, failed to send to {error} groups</i>")
+            chat_ids.append(dialog.chat.id)
+
+    async def send_broadcast(chat_id):
+        nonlocal done, error
+        try:
+            if m.reply_to_message:
+                await msg.copy(chat_id)
+            else:
+                await c.send_message(chat_id, msg)
+            done += 1
+            await asyncio.sleep(0.5)
+        except errors.FloodWait as f:
+            await asyncio.sleep(f.value + 1)
+            await send_broadcast(chat_id)
+        except Exception:
+            error += 1
+
+    for chat_id in chat_ids: # loop
+        await send_broadcast(chat_id)
+    return await load.edit(f"<i>Broadcast was sent to {done} groups, failed to send to {error} groups(s)</i>")
+
 
 @Client.on_message(filters.command("ucast", config.prefix) & filters.me)
 async def broadcast_users(c: Client, m: types.Message):
@@ -51,21 +61,31 @@ async def broadcast_users(c: Client, m: types.Message):
         return await m.reply("Give me a message or reply to a message!")
     load = await m.reply("Broadcast users processing.....")
     done = error = 0
+    chat_ids = []
+
     async for dialog in c.get_dialogs():
         if dialog.chat.type in enums.ChatType.PRIVATE:
-            chat_id = dialog.chat.id
-            try:
-                if m.reply_to_message:
-                    await msg.copy(chat_id)
-                else:
-                    await c.send_message(chat_id, msg)
-                done += 1
-                await asyncio.sleep(0.5)
-            except errors.FloodWait as f:
-                await asyncio.sleep(f.value)
-            except Exception:
-                error += 1
-    return await load.edit(f"<i>Broadcast was send to {done} users, failed to send to {error} users</i>")
+            chat_ids.append(dialog.chat.id)
+
+    async def send_broadcast(chat_id):
+        nonlocal done, error
+        try:
+            if m.reply_to_message:
+                await msg.copy(chat_id)
+            else:
+                await c.send_message(chat_id, msg)
+            done += 1
+            await asyncio.sleep(0.5)
+        except errors.FloodWait as f:
+            await asyncio.sleep(f.value + 1)
+            await send_broadcast(chat_id)
+        except Exception:
+            error += 1
+
+    for chat_id in chat_ids: # loop
+        await send_broadcast(chat_id)
+    return await load.edit(f"<i>Broadcast was send to {done} users, failed to send to {error} users(s)</i>")
+
 
 @Client.on_message(filters.command("fwdcast", config.prefix) & filters.me)
 async def broadcast_forward(c: Client, m: types.Message):
@@ -73,15 +93,24 @@ async def broadcast_forward(c: Client, m: types.Message):
         return await m.reply("Reply to messages you want to continue posting")
     load = await m.reply("Broadcast forward processing....")
     done = error = 0
+    chat_ids = []
+
     async for dialog in c.get_dialogs():
         if dialog.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
-            chat_id = dialog.chat.id
-            try:
-                await c.send_message(chat_id, reply)
-                done += 1
-                await asyncio.sleep(0.5)
-            except errors.FloodWait as f:
-                await asyncio.sleep(f.value)
-            except Exception:
-                error += 1
-    return await load.edit(f"<i>Broadcast was send to {done} forward, failed to send to {error} forward")
+            chat_ids.append(dialog.chat.id)
+
+    async def send_broadcast(chat_id):
+        nonlocal done, error
+        try:
+            await m.reply_to_message.forward(chat_id)
+            done += 1
+            await asyncio.sleep(0.5)
+        except errors.FloodWait as f:
+            await asyncio.sleep(f.value + 1)
+            await send_broadcast(chat_id)
+        except Exception:
+            error += 1
+
+    for chat_id in chat_ids: # loop
+        await send_broadcast(chat_id)
+    return await load.edit(f"<i>Broadcast was send to {done} forward, failed to send to {error} forward(s)")
